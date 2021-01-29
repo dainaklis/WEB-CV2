6073c7fd1076ca5bc492da53bad09f1464fe7059
Dainius Karpavicius <60507404+dainaklis@users.noreply.github.com>
2021-01-29 18:39:07 +0200

    Add files via upload

---
+++ ManoWeb2/index.php
@@ -0,0 +1,252 @@
+
+<?php include "header.php" ?>   
+
+    <!-------------------------- header section ---------------------------->
+
+    <section id="header">
+        <div class="container text-center">
+            <div class="user-box">
+                <img src="images/user1.jpg">
+                <h1>Dainius Karpavicius</h1>
+                <p>Samdomas darbuotojas</p>
+            </div>
+        </div>
+        <div class="scroll-btn">
+            <div class="scroll-bar"><a href="#about"><span></span></a></div>
+        </div>
+    </section>
+
+    <!-------------------------- user info section ---------------------------->
+
+    <section id="user-info">
+        <div class="nav-bar">
+            <nav class="navbar navbar-expand-lg ">
+                    <a class="navbar-brand" href="#"><img src="images/logo.png" ></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#top">Pagrindinis </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about">Apie Mane</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#resume">Aprasymas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#services">Paslaugos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Kontaktai</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+        <!-------------------------- about ---------------------------->
+
+        <div class="about container" id="about">
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <img src="images/user3.jpg" class="profile-img">
+                </div>
+                <div class="col-md-6">
+                    <h3>Kas as Esu?</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique cumque nemo officiis quasi architecto officia saepe accusamus quam, dignissimos unde quis rerum. Esse mollitia dolorum, deserunt est eos id quis!</p>
+
+                    <div class="skills-bar">
+                        <p>Adobe Photoshop</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 80%">80%</div>
+                        </div>
+
+                        <p>Wordpress</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 85%">85%</div>
+                        </div>
+
+                        <p>UI desingn</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 70%">70%</div>
+                        </div>
+
+                        <p>Graphics Design</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width: 50%">50%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!----------------------------- social media ---------------------------------->
+        <div class="social-icons">
+            <ul>
+                <a href=""><li><i class="fa fa-linkedin"></i></li></a>
+                <a href=""><li><i class="fa fa-twitter"></i></li></a>
+                <a href=""><li><i class="fa fa-medium"></i></li></a>
+                <a href=""><li><i class="fa fa-github"></i></li></a>
+                <a href=""><li><i class="fa fa-behance"></i></li></a> 
+   
+            </ul>
+        </div>
+        <!--------------------------------- Resume----------------- -->
+
+        <div class="resume" id="resume">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="text-center">Mano patirtis</h3>
+                        <ul class="timeline">
+                            <li>
+                                <h4> <span> 2019 - </span> Front End Developer</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas sed ut magnam dolorem libero nulla veritatis facere expedita nobis molestiae laboriosam amet repellat, voluptatibus fuga explicabo quis nihil voluptas fugiat. <br> 
+                                    <b>Company</b> - ABC company <br>
+                                    <b>Duration</b> - 1 yr [2019 - 2020] <br>
+                                    <b>Location</b> - Vilnius 
+                                </p>
+                            </li>
+
+                            <li>
+                                <h4><span> 2018 - </span>Front End Developer</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas sed ut magnam dolorem libero nulla veritatis facere expedita nobis molestiae laboriosam amet repellat, voluptatibus fuga explicabo quis nihil voluptas fugiat. <br> 
+                                    <b>Company</b> - ABC company <br>
+                                    <b>Duration</b> - 1 yr [2019 - 2020] <br>
+                                    <b>Location</b> - Vilnius 
+                                </p>
+                            </li>
+
+                            <li>
+                                <h4><span> 2017 - </span> Front End Developer</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas sed ut magnam dolorem libero nulla veritatis facere expedita nobis molestiae laboriosam amet repellat, voluptatibus fuga explicabo quis nihil voluptas fugiat. <br> 
+                                    <b>Company</b> - ABC company <br>
+                                    <b>Duration</b> - 1 yr [2019 - 2020] <br>
+                                    <b>Location</b> - Vilnius 
+                                </p>
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <div class="col-md-6">
+                        <h3 class="text-center">Mano issilavinimas</h3>
+                        <ul class="timeline">
+                            <li>
+                                <h4> <span> 2015 - </span> Programer school</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas sed ut magnam dolorem libero nulla veritatis facere expedita nobis molestiae laboriosam amet repellat, voluptatibus fuga explicabo quis nihil voluptas fugiat. <br> 
+                                    <b>Institute</b> - ABC company <br>
+                                    <b>Session</b> - 2015 - 2016 <br>
+                                    <b>Aggregate</b> - 80% 
+                                </p>
+                            </li>
+
+                            <li>
+                                <h4><span> 2014 - </span>Aukstasis Bakalauras</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas sed ut magnam dolorem libero nulla veritatis facere expedita nobis molestiae laboriosam amet repellat, voluptatibus fuga explicabo quis nihil voluptas fugiat. <br> 
+                                    <b>Institute</b> - PPP company <br>
+                                    <b>Session</b> - 2014 - 2015 <br>
+                                    <b>Aggregate</b> - 80% 
+                                </p>
+                            </li>
+
+                            <li>
+                                <h4><span> 2010 - </span> Vidurinis</h4>
+                                <p>
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas sed ut magnam dolorem libero nulla veritatis facere expedita nobis molestiae laboriosam amet repellat, voluptatibus fuga explicabo quis nihil voluptas fugiat. <br> 
+                                    <b>Institute</b> - BEF company <br>
+                                    <b>Session</b> - 2010 - 2014 <br>
+                                    <b>Aggregate</b> - 80%  
+                                </p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!----------------------------------- paslaugos -------------------------------------------->
+
+        <div class="services" id="services">
+            <div class="container">
+                <h1 class="text-center">Mano paslaugos</h1>
+                <p class="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus placeat quo doloremque <br> libero esse porro sapiente natus reprehenderit est fugiat similique ipsa assumenda mollitia unde, aliquid, iste laborum amet voluptatibus.</p>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="services-box">
+                            <i class="fa fa-rocket"></i><span>Graphics</span>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste asperiores optio minima impedit doloribus iure eum similique necessitatibus facere quasi quia, nemo quod ipsam quam ducimus ratione, eligendi officia repellat?</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="services-box">
+                            <i class="fa fa-line-chart"></i><span>Marketing</span>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste asperiores optio minima impedit doloribus iure eum similique necessitatibus facere quasi quia, nemo quod ipsam quam ducimus ratione, eligendi officia repellat?</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="services-box">
+                            <i class="fa fa-laptop"></i><span>Development</span>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste asperiores optio minima impedit doloribus iure eum similique necessitatibus facere quasi quia, nemo quod ipsam quam ducimus ratione, eligendi officia repellat?</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <!------------------------------ kontaktai ---------------------------------------->
+        <div class="contact" id="contact">
+            <div class="container text-center">
+                <h1>Susisiekite su manimi</h1>
+                <p class="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus placeat quo doloremque libero esse porro sapiente natus reprehenderit est fugiat similique ipsa assumenda mollitia unde, aliquid, iste laborum amet voluptatibus.</p>
+
+                <div class="row">
+
+                    <div class="col-md-4">
+                        <i class="fa fa-phone"></i>
+                        <p>+370 687 23755</p>
+                    </div>
+
+                    <div class="col-md-4">
+                        <i class="fa fa-envelope"></i>
+                        <p>dainaklis@gmail.com</p>
+                    </div>
+
+                    <div class="col-md-4">
+                        <i class="fa fa-internet-explorer"></i>
+                        <p>www.dainiuskarpavicius.lt</p>
+                    </div>
+                </div>
+
+                <button type="button" class="btn btn-primary"><i class="fa fa-download"></i>Aprasymas</button>
+
+                <button type="button" class="btn btn-primary"><i class="fa fa-rocket"></i>Pasamdykite Mane</button>
+
+            </div>
+
+            <div class="footer text-center">
+                <p>Made with <i class="fa fa-heart-o"></i> by Dainius Karpavicius</p>
+            </div>
+        </div>
+
+    </section>
+
+
+
+
+
+
+<?php include "footer.php" ?>
+
